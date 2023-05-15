1a3ecc0330906d780f0334bd983080ccd2bb1063
ramses2099 <ramses2099@gmail.com>
2023-05-15 16:22:20 -0400

    add new data folder

--- README.md
+++ README.md
@@ -2,3 +2,23 @@
 Python OS Module - Python for DevOps Chanel Youtube by Prashant Lakhera
 
 ### url https://www.youtube.com/watch?v=gOOYl8I_G7
+
+### create script using module os
+
+| Command     |  Description                                    |
+|-------------|-------------------------------------------------|
+| os.system() | use for execute command of the system operative |
+| os.getcwd() | use for get the current path                    |
+| os.chdir()  | use for change directory                        |
+| os.listdir()| use for list all directory, path is a param     |
+| os.mkdir()  | create directory                                |
+| os.rmdir()  | remove directory                                |
+
+
+### subprocess Module
+sp = subprocess.Popen(command, shell=True/False, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+command
+shell=True/False
+stdout=subprocess.PIPE
+stderr=subprocess.PIPE
+universal_newlines=True
--- main.py
+++ main.py
@@ -1,12 +1,41 @@
 import os
+import subprocess
+
 
 def main():
     # path = os.getcwd()
     # print("{}".format(path))
     # code = os.system("dir")
     # print("code: {}".format(code))
-    for file in os.listdir():
-        print(file)
+    # for file in os.listdir():
+    #     print(file)
+
+    # filename= 'README.md'
+
+    # if os.path.exists(filename):
+    #     print("File Exists")
+    #     if os.path.isdir(filename):
+    #         print("It's a directory")
+    #     else:
+    #         print("It's a file")
+    # else:
+    #     print("File not found")
+
+    # for _, _, files in os.walk("data"):
+    #     for file in files:
+    #         print(file)
+    command = "dir"
+    sp = subprocess.Popen(
+        command,
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        universal_newlines=True,
+    )
+    rt = sp.wait()
+    out, err = sp.communicate()
+    print(out)
+
 
 if __name__ == "__main__":
     main()
